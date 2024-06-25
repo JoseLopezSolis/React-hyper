f8ffc56358d7987a82588787703845fc93b201bb
joselopezsolis <pepeboss114@gmail.com>
2024-06-25 12:39:16 -0600

    hook with react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "./components/Layout/Header"
+import Contador from "./components/Contador"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Contador/>
+    </div>
+  );
+}
+
+export default App;
